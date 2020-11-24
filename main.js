df1613d41665724216e6e1948a95d805eac3bf7d
realtanyou <tan.y@outlook.sg>
2020-11-24 10:34:00 +0800

    test chess game

--- main.js
+++ main.js
@@ -1,177 +1,247 @@
-// setup canvas
-
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 const scoreCounter = document.querySelector('p');
 
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
+const width = canvas.width = 800;
+const height = canvas.height = 800;
 
+var startx; //to check on mouse move
+var starty;
+var selectedpiecex = -1, selectedpiecey = -1,selectedpiececolor = "";
 
-// function to generate random number
-
-function random(min, max) {
-  const num = Math.floor(Math.random() * (max - min + 1)) + min;
-  return num;
+let tile_size = 100;
+let tile_color = 'rgba(255,255,255,0.5)'; //white
+let white = 'rgba(255,255,255,0.5)', black = 'rgba(0,0,0,0.5)';
+function setup(){
+  ctx.fillStyle = 'rgba(255,255,255, 0.25)';
+  ctx.fillRect(0, 0, width, height);
 }
 
-class Shape {
-  constructor (x,y,velX,velY){
-    this.x = x;
-    this.y = y;
-    this.velX = velX;
-    this.velY = velY;
-    this.exists = true;
+function draw(){
+  ctx.clearRect(0,0,width,height);
+  showGrid();
+  cb.show_board();
+}
+
+function showGrid(){
+  for(var i = 0; i < 8; i++){
+    for(var j = 0; j < 8; j++){
+      if((i+j)%2 == 0) //white
+      ctx.fillStyle = white;
+      else ctx.fillStyle = black;
+      ctx.fillRect(i*tile_size,j*tile_size,tile_size,tile_size);
+    }
   }
 }
 
-class Ball extends Shape {
-  constructor (x, y, velX, velY, color, size){
-    super(x,y,velX,velY);
-    this.color = color;
-    this.size = size;
-  }
+function ispiececlicked(x,y,piece){
+  /**
+   * check that if the piece is within the same location as the click.
+   * 
+   */
+  //console.log("x is" + piece.x*tile_size);
+  //console.log("y is" + piece.y*tile_size);
+  //console.log("width is" + piece.width);
+  //console.log("heght is" + piece.height);
+  return(x >= piece.drawnx && x <= piece.drawnx + tile_size && y <= piece.drawny + tile_size && y >= piece.drawny);
 }
 
-Ball.prototype.draw = function() {
-  ctx.beginPath();
-  ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-  ctx.fill();
-}
-
-Ball.prototype.update = function() {
-  if ((this.x + this.size) >= width) {
-    this.velX = -(this.velX);
-  }
-
-  if ((this.x - this.size) <= 0) {
-    this.velX = -(this.velX);
-  }
-
-  if ((this.y + this.size) >= height) {
-    this.velY = -(this.velY);
-  }
-
-  if ((this.y - this.size) <= 0) {
-    this.velY = -(this.velY);
-  }
-
-  this.x += this.velX;
-  this.y += this.velY;
-}
-
-Ball.prototype.collisionDetect = function() {
-  for (let j = 0; j < balls.length; j++) {
-    if (!(this === balls[j])) {
-      const dx = this.x - balls[j].x;
-      const dy = this.y - balls[j].y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
-
-      if (distance < this.size + balls[j].size) { //collided
-        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+function getpiececlicked(event){
+  /**
+   * note that in a browser, x is vertical, y is horizontal. I filp this to the standard for chess.
+   */
+  console.log("get piece");
+  event.preventDefault();
+  startx = event.clientX;
+  starty = event.clientY; 
+  console.log(event.clientX + "," + event.clientY);
+  for(var j = 0; j < cb.board.length; j++){
+    for(var i = 0; i < cb.board[j].length; i++){
+      //console.log(cb.board[j][i].name);
+      if(ispiececlicked(startx,starty,cb.board[j][i])){
+        console.log("clicked piece: " + cb.board[j][i].name);
+        selectedpiecex = i;
+        selectedpiecey = j;
+        selectedpiececolor = cb.board[j][i].colour;
       }
     }
   }
+
 }
 
-class EvilCircle extends Shape {
-  constructor(x,y){
-    super(x,y,20,20);
-    this.color = 'white';
-    this.size = 10;
+function movepiece(event){
+  if(selectedpiecey< 0){
+    return;
   }
-  draw(){
-    ctx.beginPath();
-    ctx.lineWidth = 3;
-    ctx.strokeStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.stroke();
-  }
-  checkBounds(){
-    if ((this.x + this.size) >= width) {
-      this.x -= 1;
-    }
+  event.preventDefault();
+  console.log("move piece");
+  var dx = event.clientX - startx;
+  var dy = event.clientY - starty;
+  starty = event.clientY;
+  startx = event.clientX;
+  cb.board[selectedpiecey][selectedpiecex].drawnx += dx;
+  cb.board[selectedpiecey][selectedpiecex].drawny += dy;
+  draw();
   
-    if ((this.x - this.size) <= 0) {
-      this.x +=1 
-    }
+}
+
+function releasepiece(event){ //for mouse up
+  event.preventDefault();
+  console.log("release piece");
+  /**
+   * lock the piece into the grid location. update cb.board and cb.white and/or black
+   * ensure that there isn't it's own piece there, nor it is jumping over obstacles.
+   */
   
-    if ((this.y + this.size) >= height) {
-      this.y -=1;
+  //check if there is a piece in the same location as where it will be released. if it is the same colour, return to its original location.
+  //otherwise, continue
+  let finalx = Math.floor(event.clientX/tile_size);
+  let finaly = Math.floor(event.clientY/tile_size);
+  let anotherpiece = false;
+  if(typeof cb.board[finaly][finalx] != "number"){
+    //another piece is at the location.
+    console.log("piece here");
+    anotherpiece = true;
+    if(cb.board[finaly][finalx].colour == cb.board[selectedpiecey][selectedpiecex].colour){
+      //same colour, call return piece and exit
+      console.log("another same colour piece at location. return.")
+      returnpiecetoOriginal(event);
+      return;
+    } 
+  }
+  /**
+   * for all pieces, check if their moves are legit.
+   * After that, check if there are other pieces blocking their movement (except knight because it can jump over, and king as it can only move one space anyway).
+   */
+
+  let jdiff = (finaly - selectedpiecey)/Math.abs(selectedpiecey - finaly);
+  let idiff = (finalx - selectedpiecex)/Math.abs(selectedpiecex - finalx);
+  let successfulenpassant = false;
+  if (isNaN(idiff)) idiff = 0;
+  if (isNaN(jdiff)) jdiff = 0;
+  if(cb.board[selectedpiecey][selectedpiecex] instanceof pawn){
+    console.log("checking pawn move legit");
+    if(cb.board[selectedpiecey][selectedpiecex].forwardmove(finalx,finaly) && !anotherpiece){ //forward movement
+      console.log("can move forward.");
     }
-  
-    if ((this.y - this.size) <= 0) {
-      this.y +=1;
+    else if(cb.board[selectedpiecey][selectedpiecex].diagonalmove(finalx,finaly) && anotherpiece){ //diagonal capture
+      console.log("can capture diagnonal");
+    }
+    else if(cb.board[selectedpiecey][selectedpiecex].checkenpassant(finalx,finaly,cb.whiteenpassant) || cb.board[selectedpiecey][selectedpiecex].checkenpassant(finalx,finaly,cb.blackenpassant)){//en passant
+      console.log("en passant");
+      successfulenpassant = true;
+      console.log(successfulenpassant);
+    }
+    else{
+      console.log("pawn movement not allowed. return");
+      returnpiecetoOriginal(event);
+      return;
     }
   }
-  setControls(){
-    let _this = this;
-    window.onkeydown = function(e) {
-    if (e.key === 'a') {
-      _this.x -= _this.velX;
-    } else if (e.key === 'd') {
-      _this.x += _this.velX;
-    } else if (e.key === 'w') {
-      _this.y -= _this.velY;
-    } else if (e.key === 's') {
-      _this.y += _this.velY;
-    }
-  }
-  }
-  collisionDetect(){
-    for (let j = 0; j < balls.length; j++) {
-      if (balls[j].exists) {
-        const dx = this.x - balls[j].x;
-        const dy = this.y - balls[j].y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
-  
-        if (distance < this.size + balls[j].size) { //collided
-          balls[j].exists = false;
+  else if(cb.board[selectedpiecey][selectedpiecex].move(finalx,finaly)){
+    if((Math.abs(selectedpiecey - finaly) > 1 || Math.abs(selectedpiecex - finalx) > 1)){
+      console.log("move legit");
+      console.log(idiff + "," + jdiff);
+      let checky = selectedpiecey + jdiff, checkx = selectedpiecex + idiff;
+      if(!(cb.board[selectedpiecey][selectedpiecex] instanceof king || cb.board[selectedpiecey][selectedpiecex] instanceof knight)){
+        while(checky != finaly || checkx != finalx){
+          console.log(checkx + "," + checky);
+          if(typeof cb.board[checky][checkx] != "number"){
+            //there is a piece blocking. return to original location.
+            console.log("other pieces blocking you. return.");
+            returnpiecetoOriginal(event);
+            return;
+          }
+          checky+= jdiff;
+          checkx+= idiff;
         }
       }
     }
   }
-}
-
-let balls = [];
-let totalBalls = 25
-while (balls.length < totalBalls) {
-  let size = random(10,20);
-  let ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the edge of the canvas, to avoid drawing errors
-    random(0 + size,width - size),
-    random(0 + size,height - size),
-    random(-7,7),
-    random(-7,7),
-    'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
-    size
-  );
-
-  balls.push(ball);
-}
-let evilcircle = new EvilCircle(random(10,width - 10),random(0 + 10,height - 10))
-evilcircle.setControls();
-scoreCounter.textContent = "Ball count: " + totalBalls;
-function loop() {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
-  ctx.fillRect(0, 0, width, height);
-  totalBalls = 0;
-  for (let i = 0; i < balls.length; i++) {
-    if(balls[i].exists){
-    balls[i].draw();
-    balls[i].update();
-    balls[i].collisionDetect();
-    totalBalls += 1;
-    }
+  else{
+    console.log("move not valid. return.");
+    returnpiecetoOriginal(event);
+    return;
   }
-  evilcircle.draw();
-  evilcircle.checkBounds();
-  evilcircle.collisionDetect();
-  scoreCounter.textContent = "Ball count: " + totalBalls;
-  requestAnimationFrame(loop);
+
+  cb.board[selectedpiecey][selectedpiecex].drawnx = finalx * tile_size; 
+  cb.board[selectedpiecey][selectedpiecex].drawny = finaly * tile_size; 
+  if(selectedpiececolor == "white"){
+    delete cb.white[cb.board[selectedpiecey][selectedpiecex].get_pos()];
+  }
+  else if (selectedpiececolor == "black"){
+    delete cb.black[cb.board[selectedpiecey][selectedpiecex].get_pos()];
+  }
+  console.log("setting piece new position and capturing piece at location, except for rook, because rooks are weird.")
+  cb.board[selectedpiecey][selectedpiecex].x = cb.board[selectedpiecey][selectedpiecex].drawnx/100;
+  cb.board[selectedpiecey][selectedpiecex].y = cb.board[selectedpiecey][selectedpiecex].drawny/100;
+
+  if(selectedpiececolor == "white"){
+    if (successfulenpassant){
+      delete cb.black[cb.blackenpassant.get_pos()];
+      cb.board[cb.blackenpassant.y][cb.blackenpassant.x] = 0;
+      console.log("deleted black pawn");
+    }
+    else if(anotherpiece) delete cb.black[cb.board[finaly][finalx].get_pos()];
+    cb.white[cb.board[selectedpiecey][selectedpiecex].get_pos()] = cb.board[selectedpiecey][selectedpiecex];
+  }
+  else if (selectedpiececolor == "black"){
+    if (successfulenpassant){
+      delete cb.white[cb.whiteenpassant.get_pos()];
+      cb.board[cb.whiteenpassant.y][cb.whiteenpassant.x] = 0;
+      console.log("deleted white pawn");
+    }
+    if(anotherpiece) delete cb.white[cb.board[finaly][finalx].get_pos()];
+    cb.black[cb.board[selectedpiecey][selectedpiecex].get_pos()] = cb.board[selectedpiecey][selectedpiecex];
+  }
+  cb.board[finaly][finalx] = cb.board[selectedpiecey][selectedpiecex];
+  cb.board[selectedpiecey][selectedpiecex] = 0;
+  //console.log(cb.board[selectedpiecey][selectedpiecex].get_pos());
+
+  selectedpiecey = -1;
+  selectedpiecex = -1;
+  selectedpiececolor = "";
+  draw();
+  cb.print_board();
 }
 
-loop();
+function returnpiecetoOriginal(event){ //if the piece moves out of the canvas, return the piece to its original location.
+  if(selectedpiecey< 0){
+    return;
+  }
+  event.preventDefault();
+  console.log("return piece");
+  console.log(cb.board[selectedpiecey][selectedpiecex].get_pos());
+  /**
+   * return the piece to its original location if it dragged out of the play area.
+   */
+  cb.board[selectedpiecey][selectedpiecex].drawnx = cb.board[selectedpiecey][selectedpiecex].x * tile_size;
+  cb.board[selectedpiecey][selectedpiecex].drawny = cb.board[selectedpiecey][selectedpiecex].y * tile_size;
+  selectedpiecey = -1;
+  selectedpiecex = -1;
+  selectedpiececolor = "";
+  draw();
+  cb.print_board();
+}
+
+canvas.onmousedown = function(event){
+  getpiececlicked(event);
+};
+
+canvas.onmousemove = function(event){
+  movepiece(event);
+};
+
+canvas.onmouseup = function(event){
+  releasepiece(event);
+};
+
+canvas.onmouseout = function(event){
+  returnpiecetoOriginal(event);
+};
+
+
+//setup();
+showGrid();
+//let np = new piece(1,1,1,1,"TN");
---
+++ main2.js
@@ -0,0 +1,364 @@
+// chess960 with ai
+// Initial position, first (bottom) row: rook, knight, bishop, queen, king, bishop, knight, and rook; second row: pawns
+
+const min_x = 0, min_y = 0, max_x = 7, max_y = 7;
+const num_to_let = {
+    0:'a',1:'b',2:'c',3:'d',4:'e',5:'f',6:'g',7:'h'
+};
+const blacktiles = [0,2,4,6], whitetiles = [1,3,5,7]; //for first row
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+}
+
+/**
+ * 0 [][][][][][][][] black here
+ * 1 [][][][][][][][] black here
+ * 2 [][][][][][][][]
+ * 3 [][][][][][][][]
+ * 4 [][][][][][][][]
+ * 5 [][][][][][][][]
+ * 6 [][][][][][][][] white here
+ * 7 [][][][][][][][] white here
+ *   0 1 2 3 4 5 6 7
+ * 
+ * note that indexes in programming assume that the positive x axis runs downwards, not upwards.
+ * when getting and setting the name of each piece, use 2 letter phrasing
+ */
+
+class piece {
+    constructor (y,x,colour, image, name){
+        this.x = x; //current position of the piece on the board
+        this.y = y;
+        this.drawnx = x * tile_size;
+        this.drawny = y * tile_size;
+        this.colour = colour; //only black or white
+        this.image = new Image(); //for visual representation of the piece'
+        this.image.src = image;
+        this.image.onload = function(){
+            ctx.drawImage(this,this.drawnx + 25,this.drawny + 50);
+        };
+        this.name = name; //rook, bishop etc
+        this.width = 0; //width and height for hitbox.
+        this.height = 0;
+        this.movecount = 0;
+    }
+    move(new_x, new_y){
+        return false;
+    }
+    get_pos(){
+        //should return in proper chess format
+        return this.y+1 + "," + num_to_let[this.x];
+    }
+    show(){
+        //ctx.font = "30px Arial";
+        //ctx.fillStyle = 'rgba(125,125,125,1)'
+        //this.width = ctx.measureText(this.name).width;
+        //this.height = 30;
+        //ctx.fillText(this.name,this.drawnx + 25,this.drawny + 50);
+        console.log(this.image.src);
+        //ctx.drawImage(this.image,this.drawnx + 25,this.drawny + 50);
+    }
+    toString(){
+        //var newtext = Text()
+        return this.name;
+    }
+}
+
+class king extends piece {
+    constructor(x,y,colour,image,name){
+        super(x,y,colour,image,name);
+    }
+    move(new_x, new_y){
+        let sucessful_move = false;
+        //king can move one square in any direction
+        if(new_x != this.x || new_y != this.y){
+            //allow the move only if the new position given is diffrent from the current position.
+            if(Math.abs(this.x - new_x) <= 1 && Math.abs(this.y - new_y) <= 1){
+                //check that the new position moved is exactly one position 
+                sucessful_move = true;
+            }
+        }
+        return sucessful_move;
+    }
+
+}
+
+class queen extends piece{
+    constructor(x,y,colour,image,name){
+        super(x,y,colour,image,name);
+    }
+    move(new_x, new_y){
+        /**
+         * queen can move vertial, horizontal and diagonal direction for any length, but not over other pieces(to be coded later)
+         */
+        let sucessful_move = false;
+        if(new_x != this.x || new_y != this.y){
+            //chack that the new_x and new_y do not exceed boundaries. also ensure that it is only in the 8 cardinal directions
+            let diff_x = Math.abs(new_x - this.x), diff_y = Math.abs(new_y - this.y);
+            if(diff_x <= max_x && diff_y <= max_y && (diff_x == diff_y || (diff_y == 0 || diff_x == 0))){
+                //check if 8 directions
+                sucessful_move = true;
+            }
+        }
+        return sucessful_move;
+        
+    }
+}
+
+class rook extends piece{
+    constructor(x,y,colour,image,name){
+        super(x,y,colour,image,name);
+    }
+    move(new_x, new_y){
+        /**
+         * rook can vertical and horizontal of any length, except over other pieces
+         * rook can perform a special move with the king called castling
+         */
+        let sucessful_move = false;
+        if(new_x != this.x || new_y != this.y){
+            //chack that the new_x and new_y do not exceed boundaries. also ensure that it is only in the 4 cardinal directions
+            let diff_x = Math.abs(new_x - this.x), diff_y = Math.abs(new_y - this.y);
+            console.log("difference: " + diff_x + "," + diff_y);
+            if(diff_x <= max_x && diff_y <= max_y && (diff_y == 0 || diff_x == 0)){
+                sucessful_move = true;
+            }
+        }
+        return sucessful_move;
+    }
+}
+
+class bishop extends piece{
+    constructor(x,y,colour,image,name){
+        super(x,y,colour,image,name);
+    }
+    move(new_x, new_y){
+        /**
+         * bishop can move diagonal direction for any length, but not over other pieces(to be coded later)
+         */
+        let sucessful_move = false;
+        if(new_x != this.x && new_y != this.y){
+            //chack that the new_x and new_y do not exceed boundaries. also ensure that it is only in the diagonal directions
+            let diff_x = Math.abs(new_x - this.x), diff_y = Math.abs(new_y - this.y);
+            if(diff_x <= max_x && diff_y <= max_y && diff_x == diff_y){
+                sucessful_move = true;
+            }
+        }
+        return sucessful_move;
+        
+    }
+}
+
+class knight extends piece{
+    constructor(x,y,colour,image,name){
+        super(x,y,colour,image,name);
+    }
+    move(new_x, new_y){
+        /**
+         * knight moves in an 'L' shape and can jump over other pieces.
+         */
+        let sucessful_move = false;
+        if(new_x != this.x && new_y != this.y){
+            //chack that the new_x and new_y do not exceed boundaries. also ensure that it is only in the 8 cardinal directions
+            let diff_x = Math.abs(new_x - this.x), diff_y = Math.abs(new_y - this.y);
+            if((diff_x == 2 && diff_y == 1) || (diff_x == 1 && diff_y == 2)){
+                sucessful_move = true;
+            }
+        }
+        return sucessful_move;
+        
+    }
+}
+
+class pawn extends piece {
+    constructor(x,y,colour,image,name){
+        super(x,y,colour,image,name);
+    }
+    forwardmove(new_x, new_y){
+        let sucessful_move = false;
+        /** 
+         * pawns can only move forward, unless diagonal capture, en passant or promotion
+         * x for pawns are only used for en passant,diagonal movement forward
+         * en passant can only happend if the piece to be captured was moved there on the opponent's turn. otherwise, the right to capture it is forfieted\
+         * */ 
+        
+        //right now, code only move forward or back.
+        if(new_y != this.y && new_x == this.x){
+            let res = new_y - this.y;
+            if((this.colour == "white" && res < 0 && res >= -2) || (this.colour == "black" && res > 0 && res <= 2)){
+                sucessful_move = true;
+            }
+            if(Math.abs(res) == 2 && this.movecount == 0 && sucessful_move){
+                //first move piece, can be enpassant on.
+                if(this.colour == "white") cb.whiteenpassant = this;
+                if(this.colour == "black") cb.blackenpassant = this;
+                console.log(cb.whiteenpassant);
+                console.log(cb.blackenpassant);
+                this.movecount+=1;
+            }
+        }
+        return sucessful_move;
+    }
+    diagonalmove(new_x,new_y){
+        let sucessful_move = false;
+        if(new_y!= this.y && new_x != this.x){
+            let resy = new_y - this.y, resx = Math.abs(new_x - this.x);
+            if(resx == 1 && ((this.colour == "white" && resy == -1) || (this.colour == "black" && resy == 1))){
+                sucessful_move = true;
+            }
+        }
+        return sucessful_move;
+    }
+    checkenpassant(newx,newy,otherpawn){
+        let sucessful_move = false;
+        if(this.diagonalmove(newx,newy)){
+            console.log("can move diagonal");
+            if(otherpawn instanceof pawn && pawn.colour != this.colour){
+                console.log("other piece is pawn and not of same colour");
+                if(Math.abs(otherpawn.x - this.x) == 1){
+                    //pawn is in correct position.
+                    sucessful_move = true;
+                }
+            }
+        }
+        return sucessful_move;
+    }
+}
+
+class chessboard {
+    constructor(){
+        //make an 8 by 8 chessboard, save the locations of all the chess pieces
+        this.board = [];
+        for(var i = 0; i < 8; i++){
+            this.board.push([0,0,0,0,0,0,0,0]);
+        }
+        // make 2 players: white and black
+        // they are a dictionary of location to piece.
+        //all pawns are in the same position, but for chess960, the other pieces are pseudo-randomiszed.
+        //The bishops must be placed on opposite-color squares.
+        //The king must be placed on a square between the rooks.
+        //black follows white placements.
+        this.white = {};
+        this.black = {};
+        //record which piece currently can be enpassant. intialise it to be blank.
+        this.whiteenpassant = "";
+        this.blackenpassant = "";
+
+        let availabletiles = [0,1,2,3,4,5,6,7]; // x axis
+        let white_loc = 7;
+        let black_loc = 0;
+        //randomly set the white bishops
+        let bish_ran = whitetiles[Math.floor(Math.random() * whitetiles.length)];
+        let bish = new bishop(white_loc,bish_ran,"white","images/whitebishop.png","WB");
+        this.white[bish.get_pos()] = bish;
+        this.board[white_loc][bish_ran] = bish;
+        availabletiles.splice(availabletiles.indexOf(bish_ran),1);
+        bish_ran = blacktiles[Math.floor(Math.random() * blacktiles.length)];
+        bish = new bishop(white_loc,bish_ran,"white","images/whitebishop.png","WB");
+        this.white[bish.get_pos()] = bish;
+        this.board[white_loc][bish_ran] = bish;
+        availabletiles.splice(availabletiles.indexOf(bish_ran),1);
+        //randomly set the knights
+        for(var i = 0; i < 2; i++){
+            bish_ran = availabletiles[random(0,availabletiles.length-1)];
+            bish = new knight(white_loc,bish_ran,"white","images/whiteknight.png","WK");
+            this.white[bish.get_pos()] = bish;
+            this.board[white_loc][bish_ran] = bish;
+            availabletiles.splice(availabletiles.indexOf(bish_ran),1);
+        }
+        //set the queen
+        bish_ran = availabletiles[random(0,availabletiles.length-1)];
+        bish = new queen(white_loc,bish_ran,"white","images/whitequeen.png","WQ");
+        this.white[bish.get_pos()] = bish;
+        this.board[white_loc][bish_ran] = bish;
+        availabletiles.splice(availabletiles.indexOf(bish_ran),1);
+        //set the king in the centre of both thr rooks.
+        bish_ran = availabletiles[1];
+        bish = new king(white_loc,bish_ran,"white","images/whiteking.png","WG");
+        this.white[bish.get_pos()] = bish;
+        this.board[white_loc][bish_ran] = bish;
+        availabletiles.splice(availabletiles.indexOf(bish_ran),1);
+        //finally, the rooks
+        for(var i = 0; i < 2; i++){
+            bish_ran = availabletiles[random(0,availabletiles.length-1)];
+            bish = new rook(white_loc,bish_ran,"white","images/whiterook.png","WR");
+            this.white[bish.get_pos()] = bish;
+            this.board[white_loc][bish_ran] = bish;
+            availabletiles.splice(availabletiles.indexOf(bish_ran),1);
+        }
+        //finally, add the rest of the pawns
+        for(var i = 0; i < 8; i++){
+            bish = new pawn(white_loc-1,i,"white","images/whitepawn.png","WP");
+            this.white[bish.get_pos()] = bish;
+            this.board[white_loc-1][i] = bish;
+        }
+
+        //repeat the rest for black, but for the same positions.
+        for (var key in this.white) {
+            if (this.white.hasOwnProperty(key)) {
+                let temp_pos = [black_loc,this.white[key].x];
+                switch(this.white[key].name){
+                    case "WB":
+                        bish = new bishop(temp_pos[0],temp_pos[1],"black","images/blackbishop.png","BB");
+                        break;
+                    case "WK":
+                        bish = new knight(temp_pos[0],temp_pos[1],"black","images/blackknight.png","BK");
+                        break;
+                    case "WQ":
+                        bish = new queen(temp_pos[0],temp_pos[1],"black","images/blackqueen.png","BQ");
+                        break;
+                    case "WG":
+                        bish = new king(temp_pos[0],temp_pos[1],"black","images/blackking.png","BG");
+                        break;
+                    case "WR":
+                        bish = new rook(temp_pos[0],temp_pos[1],"black","images/blackrook.png","BR");
+                        break;
+                    case "WP":
+                        temp_pos[0] = 1;
+                        bish = new pawn(temp_pos[0],temp_pos[1],"black","images/blackpawn.png","BP");
+                        break;
+                    default:
+                        break;
+                }
+                this.board[temp_pos[0]][temp_pos[1]] = bish;
+                this.black[bish.get_pos()] = bish;
+            }
+        }
+    }
+    print_board(){
+        //prints out the pieces in a board like fashion
+        let string_board = "";
+        for(var j = 0; j < 8; j++){
+            let board_line = j+1;
+            for(var i = 0; i < 8; i++){
+                if(typeof this.board[j][i] == "number"){
+                    board_line += "[" + "--" + "]";
+                }
+                else{
+                    board_line += "[" + this.board[j][i].name + "]";
+                }
+            }
+            string_board += board_line + '\n';
+        }
+        string_board += "  a   b   c   d   e   f   g   h"
+        console.log(string_board);
+        console.table(this.white);
+        console.table(this.black);
+    }
+    show_board(){
+        for(var i = 7; i>=0; i--){
+            for(var j = 0; j < 8; j++){
+                if(typeof this.board[i][j] == "number"){
+                }
+                else{
+                    this.board[i][j].show();
+                }
+            }
+        }
+    }
+}
+
+const cb = new chessboard();
+cb.print_board();
+cb.show_board();
+console.log("complete test");
